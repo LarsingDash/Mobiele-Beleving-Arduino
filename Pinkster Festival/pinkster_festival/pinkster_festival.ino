48581c0b7a2ff99d371a5f0f9a45099b2ef93b53
Tygo van Eerd <tpj.vaneerd@student.avans.nl>
2022-06-01 15:24:11 +0200

    Started working on Pinkster Festival
    LED lights up with random colour (red, green or blue). Turns off after corresponding button has been pushed, or after 5 seconds. BLUE BUTTON DOESN'T WORK YET

--- Pinkster Festival/pinkster_festival/pinkster_festival.ino
+++ Pinkster Festival/pinkster_festival/pinkster_festival.ino
@@ -1,9 +1,61 @@
-void setup() {
-  // put your setup code here, to run once:
+const int red = 21;
+const int green = 22;
+const int blue = 23;
+const int rButton = 2;
+const int gButton = 4;
+const int bButton = 5;
+int rNumber;
+long currentTime;
 
+void setup() {
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(blue, OUTPUT);
+  
+  pinMode(rButton, INPUT_PULLUP);
+  pinMode(gButton, INPUT_PULLUP);
+  pinMode(bButton, INPUT_PULLUP);
+
+  Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(red, LOW);
+  digitalWrite(green, LOW);
+  digitalWrite(blue, LOW);
 
+  delay(500);
+
+  //Pick random color, display this
+  rNumber = random(3);
+  switch (rNumber){
+    case 0:
+      digitalWrite(red, HIGH);
+      Serial.println("Picked red");
+      break;
+    case 1:
+      digitalWrite(green, HIGH);
+      Serial.println("Picked green");
+      break;
+    case 2:
+      digitalWrite(blue, HIGH);
+      Serial.println("Picked blue");
+      break;
+  }
+
+  currentTime = millis();
+  while(millis() < currentTime + 5000){
+    if (rNumber == 0 && digitalRead(rButton) == HIGH){
+      Serial.println("Pressed red button");
+      break;
+    }
+    if (rNumber == 1 && digitalRead(gButton) == HIGH){
+      Serial.println("Pressed green button");
+      break;
+    }
+    if (rNumber == 2 && digitalRead(bButton) == HIGH){
+      Serial.println("Pressed blue button");
+      break;
+    }
+  }
 }
