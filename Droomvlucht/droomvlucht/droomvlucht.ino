c4996fb89f382c1c2acf2f4eff9584bf18d5cf60
Tygo van Eerd <tpj.vaneerd@student.avans.nl>
2022-06-02 15:06:25 +0200

    Created link between ultrasone and servo

--- Droomvlucht/droomvlucht/droomvlucht.ino
+++ Droomvlucht/droomvlucht/droomvlucht.ino
@@ -1,9 +1,37 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <ESP32Servo.h>
 
+#include <afstandssensor.h>
+
+
+AfstandsSensor sensor(13, 12);
+Servo servo;
+
+void setup() {
+  Serial.begin(115200);
+  servo.attach(32, 500, 2400);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (random(0, 100) == 0) {
+    servo.write(100);
+    while(getDistance() > 10){
+      delay(100);
+      Serial.println(getDistance());
+    }
+    servo.write(0);
+  }
 
+  Serial.println(getDistance());
+  
+  delay(100);
+}
+
+double getDistance(){
+  double distance = sensor.afstandCM();
+  
+  if (distance < 0){
+    distance = 42;
+  }
+
+  return distance;
 }
