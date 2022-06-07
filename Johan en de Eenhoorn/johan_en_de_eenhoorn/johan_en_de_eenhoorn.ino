aae952cb908514860d89793faa6cbdce31f94f13
Tygo van Eerd <tpj.vaneerd@student.avans.nl>
2022-06-07 19:48:02 +0200

    Hit logic works, but buttons seem to be always pressed?

--- Johan en de Eenhoorn/johan_en_de_eenhoorn/johan_en_de_eenhoorn.ino
+++ Johan en de Eenhoorn/johan_en_de_eenhoorn/johan_en_de_eenhoorn.ino
@@ -4,13 +4,14 @@
 
 const int atkButton = 12;
 const int defButton = 13;
-const int frameTime = 2000;
+const int frameTime = 1000;
+const int playTime = 30000;
 const int spawnChance = 2;
 int score = 0;
 long timer;
 
-int atkPos = random(8, 16);
-int defPos = random(0, 7);
+int atkPos = random(11, 16);
+int defPos = random(0, 5);
 
 //byte attack[] = {0b00100,0b00100,0b00100,0b00100,0b00100,0b10101,0b01110,0b00100};
 //byte defend[] = {0b11111,0b11111,0b11111,0b11111,0b11111,0b01110,0b01110,0b00100};
@@ -18,8 +19,8 @@ int defPos = random(0, 7);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup() {
-  pinMode(atkButton, INPUT_PULLUP);
-  pinMode(defButton, INPUT_PULLUP);
+  pinMode(atkButton, OUTPUT);
+  pinMode(defButton, OUTPUT);
 
 //  lcd.createChar(0, attack);
 //  lcd.createChar(1, defend);
@@ -34,7 +35,7 @@ void setup() {
 }
 
 void loop() {
-  //Initialize display
+  //Reset display
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Scan phone");
@@ -50,57 +51,83 @@ void loop() {
   }
 
   Serial.println("Activated, now playing");
-  //Play game for 30 seconds
+  //Play game for however long playTime is set to
   timer = millis();
-  while(millis() < timer + 30000){
+  while(millis() < timer + playTime){
     play();
     delay(frameTime);
+
+    
+    if (digitalRead(atkButton == HIGH)){
+      Serial.println("atkButton is pressed");
+    }
+
+    if (digitalRead(defButton == HIGH)){
+      Serial.println("defButton is pressed");
+    }
   }
 }
 
 void play(){
-  if (atkPos == 8){
-    if (digitalRead(atkButton == HIGH)){
-      Serial.println("Succesfully attacked, added 1 point");
-      score++;
-    }
-    atkPos = 100;
-  } else if (atkPos == 100){
-    if (random(0, spawnChance) == 0){
-      atkPos = 15;
-    } else {
+  //Analyze attack position
+  switch (atkPos) {
+    case 8:
+      //If at line (x = 8), determine whether corresponding button is pressed
+      if (digitalRead(atkButton == HIGH)){
+        score++;
+        Serial.println("Successfully attacked, added 1 point");
+      }
+
+      //Set attack position to storage value to despawn character from LCD
       atkPos = 100;
-    }
-  } else {
-    atkPos--;
+      break;
+    case 100:
+      //If attack position is in storage, roll for chance to spawn
+      if (random(0, spawnChance) == 0){
+        atkPos = 15;
+      }
+      break;
+    default:
+      //If none of the above (meaning attack position is 15, 14, 13, 12, 10 or 9), decrease position
+      atkPos--;
+      break;
   }
 
-  
-  if (defPos == 6){
-    if (digitalRead(defButton == HIGH)){
-      Serial.println("Succesfully defended, added 1 point");
-      score++;
-    } else {
-      Serial.println("Unsuccesfully defend, subtracted 1 point");
-      score--;
-    }
-    defPos = 100;
-  } else if (defPos == 100){
-    if (random(0, spawnChance) == 0){
-      defPos = 0;
-    } else{
-      atkPos = 100;
-    }
-  } else {
-    atkPos++;
+  //Analyze defend position
+  switch (defPos) {
+    case 6:
+      //If at line (x = 6), determine whether corresponding button is pressed
+      if (digitalRead(defButton == HIGH)){
+        score++;
+        Serial.println("Successfully defended, added 1 point");
+      } else {
+        score--;
+        Serial.println("Unsuccessfully defended, subtracted 1 point");
+      }
+
+      //Set defend position to storage value to despawn character from LCD
+      defPos = 100;
+      break;
+    case 100:
+    //If defend position is in storage, roll for chance to spawn
+      if (random(0, spawnChance) == 0){
+        defPos = 0;
+      }
+      break;
+    default:
+      //If none of the above (meaning defend position is 0, 1, 2, 3, 4 or 5), increase position
+      defPos++;
+      break;
   }
-  
+
+  //Draw on LCD
   updateLCD();
 }
 
 void updateLCD(){
   lcd.clear();
 
+  //Draw game elements
   lcd.setCursor(6, 0);
   lcd.print("V");
   lcd.setCursor(8, 0);
@@ -110,10 +137,9 @@ void updateLCD(){
   lcd.setCursor(7, 1);
   lcd.print("I");
   
- 
+  //Draw attack and/or defense on current position
   lcd.setCursor(defPos, 1);
   lcd.print("D");
-
   lcd.setCursor(atkPos, 1);
   lcd.print("A");
 }
