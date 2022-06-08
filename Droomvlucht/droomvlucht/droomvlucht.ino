8de13472b36ec469328a8154b668d12688e803be
Tygo van Eerd <tpj.vaneerd@student.avans.nl>
2022-06-08 13:25:29 +0200

    Started on Droomvlucht

--- Droomvlucht/droomvlucht/droomvlucht.ino
+++ Droomvlucht/droomvlucht/droomvlucht.ino
@@ -2,22 +2,28 @@
 
 #include <afstandssensor.h>
 
-
+//Initialize afstandssensor
 AfstandsSensor sensor(13, 12);
 Servo servo;
 
 void setup() {
-  Serial.begin(115200);
   servo.attach(32, 500, 2400);
+  Serial.begin(115200);
 }
 
 void loop() {
+  //Randomly activate (1 in 100 every 100 ms = once every 10 seconds)
   if (random(0, 100) == 0) {
+    //Servo up
     servo.write(100);
-    while(getDistance() > 10){
+
+    //Wait for distance to get between 0 and 10, check every 100 ms
+    while(!(getDistance() < 10 && getDistance() > 0)){
       delay(100);
       Serial.println(getDistance());
     }
+
+    //Servo down
     servo.write(0);
   }
 
@@ -27,10 +33,12 @@ void loop() {
 }
 
 double getDistance(){
+  //Retrieve distance
   double distance = sensor.afstandCM();
-  
-  if (distance < 0){
-    distance = 42;
+
+  //If distance is 0 or lower, set to 0
+  if (distance < 1){
+    distance = 0
   }
 
   return distance;
