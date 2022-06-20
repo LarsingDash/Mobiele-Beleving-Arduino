60dec50bdf887ad1bb826cb8070026990981e6dd
Tygo van Eerd <tpj.vaneerd@student.avans.nl>
2022-06-20 14:09:46 +0200

    Game interacts with server

--- Pinkster Festival/pinkster_festival/pinkster_festival.ino
+++ Pinkster Festival/pinkster_festival/pinkster_festival.ino
@@ -18,30 +18,36 @@ const int playTime = 15000;
 const int pressWindow = 5000;
 
 //Loop variables
+long playTimer;
+long windowTimer;
 int score = 0;
-long timer;
-long timer2;
 int rNumber;
 
+bool isAvailable;
+int landPoints;
+int userPoints;
+String userLand;
+
 //Wifi
-const char* WLAN_ssid = "MQTT Test Network";
-const char* WLAN_access_key = "00177013";
+const char* ssid = "MQTT Network";
+const char* access_key = "00177013";
 
 //Client IP
-const char* MQTT_client_id = "arduino_festival_overal";
+const char* client_id = "arduino_festival_overal";
 
 //MQTT broker
-const char* MQTT_broker_url = "broker.hivemq.com";
-const int   MQTT_port       = 1883;
-const char* MQTT_username   = "";
-const char* MQTT_password   = "";
+const char* broker_url = "broker.hivemq.com";
+const int   port       = 1883;
+const char* username   = "";
+const char* password   = "";
 
 //MQTT topics
-const char* MQTT_topic_games_isAvailable = "esstelstrijd/games/fest/isAvailable";
-const char* MQTT_topic_games_currentUser = "esstelstrijd/games/fest/currentUser";
-
-//MQTT quality of service
-const int MQTT_qos = 0;
+const String games_fest_isAvailable   = "esstelstrijd/games/fest/isAvailable";
+const String games_fest_currentUser   = "esstelstrijd/games/fest/currentUser";
+String users_user                     = "esstelstrijd/users/";
+String lands_land                     = "esstelstrijd/lands/";
+const String points_END               = "/points";
+const String users_user_land_END      = "/land";
 
 //Create Wifi and MQTT variables
 WiFiClient wifi;
@@ -50,17 +56,62 @@ PubSubClient mqtt(wifi);
 //LCD
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-//Setup callback
-void mqttCallback(char* topic, byte* payload, unsigned int length){
+//Callback
+void callback(char* topic, byte* payload, unsigned int length){
   //Logging
-  Serial.println("--------------------------------");
-  Serial.println("MQTT callback called for topic: ");
+  Serial.println("-------------------------------");
+  Serial.print("MQTT callback called for topic: ");
   Serial.println(topic);
-  Serial.println("Message: ");
-  for (int i = 0; i < length; i++){
-    Serial.print((char)payload[i]);
+
+  char messageBuffer[30]; //Buffer to hold string
+  memcpy(messageBuffer, payload, length); //Copy payload to buffer
+  messageBuffer[length] = '\0'; // Terminate array to transform buffer into string
+  String message = messageBuffer; // Write to permanent variable
+
+  //Action
+  if(!strcmp(topic, (char*)games_fest_isAvailable.c_str())){ // Topic filter
+    if(message.equals("no") && isAvailable != false){ // Message filter
+      isAvailable = false; // Action
+      Serial.println("Set isAvailable_local to false");
+    }else if(message.equals("yes") && isAvailable != true){
+      isAvailable = true;
+      Serial.println("Set isAvailable_local to true");
+    }
+  }else if(!strcmp(topic, (char*)games_fest_currentUser.c_str())){
+    users_user = users_user + message;
+    Serial.print("Received a user: ");
+    Serial.println(users_user);
+
+    mqtt.subscribe((char*)(users_user + points_END).c_str());
+    Serial.println("Subscribed to user points");
+    mqtt.unsubscribe((char*)(users_user + points_END).c_str());
+    Serial.println("Unsubscribed to user points");
+    mqtt.loop();
+
+    mqtt.subscribe((char*)(users_user + users_user_land_END).c_str());
+    Serial.println("Subscribed to user land");
+    mqtt.unsubscribe((char*)(users_user + users_user_land_END).c_str());
+    Serial.println("Unsubscribed to user land");
+    mqtt.loop();
+    
+  } else if(!strcmp(topic, (char*)(users_user + points_END).c_str())){
+    userPoints = message.toInt();
+    Serial.print("Received user points: ");
+    Serial.println(userPoints);
+  } else if(!strcmp(topic, (char*)(users_user + users_user_land_END).c_str())){
+    userLand = message;
+    Serial.print("Received user land: ");
+    Serial.println(userLand);
+  } else if(!strcmp(topic, (char*)(lands_land + userLand + points_END).c_str())){
+    landPoints = message.toInt();
+    Serial.print("Received land points: ");
+    Serial.println(landPoints);
+    Serial.print("User score: ");
+    Serial.println(score);
+
+    publishRetain((users_user + points_END), String(userPoints + score));
+    publishRetain((lands_land + userLand + points_END), String(landPoints + score));
   }
-  Serial.println();
 }
 
 void setup() {
@@ -84,47 +135,62 @@ void setup() {
   //WiFi
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
-  Serial.println("Connecting to ");
-  Serial.println(WLAN_ssid);
-  WiFi.begin(WLAN_ssid, WLAN_access_key);
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
+  WiFi.begin(ssid, access_key);
   
   while(WiFi.status() != WL_CONNECTED){
     Serial.print(".");
-    delay(1000);
+    delay(250);
   }
 
   Serial.println("WiFi connected");
-  Serial.println("IP address: ");
+  Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
     
   //MQTT
   //Setup client
-  mqtt.setServer(MQTT_broker_url, MQTT_port);
-  mqtt.setCallback(mqttCallback);
+  mqtt.setServer(broker_url, port);
+  mqtt.setCallback(callback);
   
   //Connect to broker
-  if(mqtt.connect(MQTT_client_id, MQTT_username, MQTT_password)){
+  if(mqtt.connect(client_id, username, password)){
     Serial.println("Connected to MQTT broker");
   }else{
     Serial.println("Failed to connect to MQTT broker");
   }
 
-  //Subscribe to topic
-  if(mqtt.subscribe(MQTT_topic_games_isAvailable, MQTT_qos)){
-    Serial.println("Subscribed to ");
-    Serial.println(MQTT_topic_games_isAvailable);
+  //Subscribe to isAvailable topic
+  if(mqtt.subscribe((char*)games_fest_isAvailable.c_str())){
+    Serial.print("Subscribed to ");
+    Serial.println((char*)games_fest_isAvailable.c_str());
   }else{
-    Serial.println("Failed to subscribe to ");
-    Serial.println(MQTT_topic_games_isAvailable);
+    Serial.print("Failed to subscribe to ");
+    Serial.println((char*)games_fest_isAvailable.c_str());
   }
 
-  if(mqtt.subscribe(MQTT_topic_games_currentUser, MQTT_qos)){
-    Serial.println("Subscribed to ");
-    Serial.println(MQTT_topic_games_currentUser);
-  }else{
-    Serial.println("Failed to subscribe to ");
-    Serial.println(MQTT_topic_games_currentUser);
-  }
+  defaultStates();
+  delay(2500);
+}
+
+void defaultStates(){
+  score = 0;
+  userPoints = 0;
+  userLand = "";
+  landPoints = 0;
+  users_user = "esstelstrijd/users/";
+  lands_land = "esstelstrijd/lands/";
+  Serial.println("Reset everything");
+}
+
+void publishRetain(String topic, String message){
+  byte Buffer[message.length() + 1];
+  message.getBytes(Buffer, message.length() + 1);
+  mqtt.publish((char*)topic.c_str(), Buffer, message.length(), true);
+  Serial.print("Published message ");
+  Serial.print(message);
+  Serial.print(" to ");
+  Serial.println(topic);
 }
 
 void updateLCD(){
@@ -174,6 +240,7 @@ void addPoints(){
     lcd.setCursor(7, 1);
     lcd.print(50 + i);
     delay(100);
+    mqtt.loop();
   }
 }
 
@@ -183,21 +250,25 @@ void play(){
   switch (rNumber){
     case 0:
       digitalWrite(red, HIGH);
+      Serial.println("Led is now red");
       break;
     case 1:
       digitalWrite(green, HIGH);
+      Serial.println("Led is now green");
       break;
     case 2:
       digitalWrite(blue, HIGH);
+      Serial.println("Led is now blue");
       break;
   }
 
   //Active pause
-  timer2 = millis();
-  while(millis() < timer2 + pressWindow){
+  windowTimer = millis();
+  while(millis() < windowTimer + pressWindow){
     //If light is red and red button is pressed
     if (digitalRead(rButton) == HIGH){
       if (rNumber == 0){
+        Serial.println("Red button pressed");
         score++;
       }
       break;
@@ -205,6 +276,7 @@ void play(){
     //If light is green and green button is pressed
     if (digitalRead(gButton) == HIGH){
       if (rNumber == 1){
+        Serial.println("Green button pressed");
         score++;
       }
       break;
@@ -212,10 +284,13 @@ void play(){
     //If light is blue and blue button is pressed
     if (digitalRead(bButton) == HIGH){
       if (rNumber == 2){
+        Serial.println("Blue button pressed");
         score++;
       }
       break;
     }
+    
+    mqtt.loop();
   }
 
   //Reset led
@@ -228,6 +303,11 @@ void play(){
 }
 
 void loop() {
+  long bufferTimer = millis();
+  while(millis() < bufferTimer + 1000){
+    mqtt.loop();
+  }
+  
   //Reset display
   lcd.clear();
   lcd.setCursor(0, 0);
@@ -235,43 +315,42 @@ void loop() {
   lcd.setCursor(0, 1);
   lcd.print("to start game");
 
-  //Buffer
-  delay(2000);
-
   //Wait for activation
-  while(digitalRead(rButton) == LOW || digitalRead(gButton) == LOW || digitalRead(bButton) == LOW){
+  while(isAvailable){
     mqtt.loop();
   }
+
+  defaultStates();
+  mqtt.subscribe((char*)games_fest_currentUser.c_str());
+  Serial.println("Subscribed to currentUser");
+  mqtt.loop();
+  mqtt.unsubscribe((char*)games_fest_currentUser.c_str());
+  Serial.println("Unsubscribed to currentUser");
+  mqtt.loop();
 
   countDown();
   updateLCD();
 
+  Serial.println("Playing game...");
   //Play game for however long playTime is set to
-  timer = millis();
-  while(millis() < timer + playTime){
+  playTimer = millis();
+  while(millis() < playTimer + playTime){
     play();
     delay(frameTime);
+    mqtt.loop();
   }
+  Serial.println("Game finished");
 
-  //Ask to add points
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Score:");
-  lcd.setCursor(7, 0);
-  lcd.print(score);
-  lcd.setCursor(0, 1);
-  lcd.print("Add points?");
-  
-  //Wait for clickthrough
-  while(digitalRead(rButton) == LOW || digitalRead(gButton) == LOW || digitalRead(bButton) == LOW){
-  }
+  mqtt.subscribe((char*)(lands_land + userLand + points_END).c_str());
+  Serial.print("Subscribed to land: ");
+  Serial.println(userLand);
+  mqtt.unsubscribe((char*)(lands_land + userLand + points_END).c_str());
+  Serial.print("Unsubscribed to land: ");
+  Serial.println(userLand);
+  mqtt.loop();
+
+  publishRetain(games_fest_isAvailable, "yes");
+//  publishRetain(games_fest_currentUser, "default");
 
   addPoints();
-
-  //Wait for clickthrough
-  while(digitalRead(rButton) == LOW || digitalRead(gButton) == LOW || digitalRead(bButton) == LOW){
-  }
-
-  //Reset score
-  score = 0;
 }
